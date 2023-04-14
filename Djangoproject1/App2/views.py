9fc8ee041174e1e784662dbe4554ff45c55cd5c8
ramu91981 <ram91981honey@gmail.com>
2023-04-14 11:07:37 +0530

    firstcommit

---
+++ Djangoproject1/App2/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render;
+from django.http import HttpResponse;
+import datetime;
+
+#Create your views here...
+def f22(request):
+   time = datetime.datetime.now();
+   print(time)
+   msg = "<h2 style='color:Green;'>Hello User..!!<br /><br />Server Date & Time :: "+str(time)+"</h2><hr/>"
+   return HttpResponse(msg);
